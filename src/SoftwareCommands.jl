a875d74dc3855e7f979892ea58a83629b68a27e8
singularitti <singularitti@outlook.com>
2022-10-16 03:26:53 -0400

    Add types `Command` & `ComposedCommand`

--- src/SoftwareCommands.jl
+++ src/SoftwareCommands.jl
@@ -1,5 +1,13 @@
 module SoftwareCommands
 
-# Write your package code here.
+abstract type Command end
+# See https://github.com/JuliaLang/julia/blob/3fa2d26/base/operators.jl#L1078-L1083 & https://github.com/JuliaGeometry/CoordinateTransformations.jl/blob/ff9ea6e/src/core.jl#L29-L32
+struct ComposedCommand{A<:Command,B<:Command} <: Command
+    a::A
+    b::B
+end
+
+# See https://github.com/JuliaLang/julia/blob/3fa2d26/base/operators.jl#L1088
+Base.:∘(a::Command, b::Command) = ComposedCommand(a, b)
 
 end
